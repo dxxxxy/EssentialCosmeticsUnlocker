14c465b6b2cc2876d81d78acdfb40eee55ce41d2
dxxxxy <introsdream@gmail.com>
2023-03-25 16:56:52 -0400

    docs: compatibility notes

--- src/main/java/studio/dreamys/ecu/ecu.java
+++ src/main/java/studio/dreamys/ecu/ecu.java
@@ -3,5 +3,6 @@ package studio.dreamys.ecu;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("ecu")
+//COMPATIBILITY: THIS FILE SHOULDN'T EXIST FOR LEGACY FORGE VERSIONS (1.8.9, 1.12.2)
 public class ecu {
 }
