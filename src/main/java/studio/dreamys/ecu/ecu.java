ebc82799bebdfae327a9db649aec6fe4700beca0
dxxxxy <introsdream@gmail.com>
2023-07-08 23:33:04 -0400

    workflow: one build compatibility

--- src/main/java/studio/dreamys/ecu/ecu.java
+++ src/main/java/studio/dreamys/ecu/ecu.java
@@ -3,6 +3,4 @@ package studio.dreamys.ecu;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod("ecu")
-//COMPATIBILITY: THIS FILE SHOULDN'T EXIST FOR LEGACY FORGE VERSIONS (1.8.9, 1.12.2)
-public class ecu {
-}
+public class ecu {}
